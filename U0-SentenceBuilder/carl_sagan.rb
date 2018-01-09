1913eabc7b68e5dbec62d787f29ccd3bb909de76
Wellington Ferro <w.ferro2@gmail.com>
2018-01-09 02:54:05 +0000

    Challenge 0 part2

---
+++ U0-SentenceBuilder/carl_sagan.rb
@@ -0,0 +1,28 @@
+# WEBD-2000 - Ultimate Challenge Zero
+#
+# Using a single puts statement build the following
+# sentence using only data from the carl hash and the
+# sagan array along with some string interpolation.
+#
+# We are a way for the cosmos to know itself.
+
+carl  = {
+          :toast => 'cosmos',
+          :punctuation => [ ',', '.', '?' ],
+          :words => [ 'know', 'for', 'we']
+        }
+
+sagan = [
+          { :are => 'are', 'A' => 'a' },
+          { 'waaaaaay' => 'way', :th3 => 'the' },
+          'itself',
+          { 2 => ['to']}
+        ]
+
+# Here is an example of building a setence out of array/hash pieces.
+example = [ 'test', 'a', 'is']
+time    = { :that => 'This', :period => '.'}
+puts "#{time[:that]} #{example[2]} #{example[1]} #{example[0]}#{time[:period]}"
+
+# Wellington Ferro 0288744
+puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
