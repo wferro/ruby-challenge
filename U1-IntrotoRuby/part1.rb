6dfd88a2193e2ae11acddb2fc0058af35fe8c39a
Wellington Ferro <w.ferro2@gmail.com>
2018-01-12 19:16:07 +0000

    Challenge1 part1

--- U1-IntrotoRuby/part1.rb
+++ U1-IntrotoRuby/part1.rb
@@ -1,5 +1,16 @@
 # Wellington Ferro 0288744
 
-arr = Array.new
-(0...20).each { arr << rand(100) }
-arr.each {|a| puts a}
+ghosts = [
+          { :name => 'Inky', :age => 4, :loves => 'reindeers', :net_worth => 25 },
+		  { :name => 'Pinky', :age => 5, :loves => 'garden', :net_worth => 14 },
+		  { :name => 'Blinky', :age => 7, :loves => 'ninjas', :net_worth => 18.03 },
+		  { :name => 'Clyde', :age => 6, :loves => 'yarn ', :net_worth => 0 }
+        ]
+
+
+ghosts.each do |ghost|
+    ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+    ghost_info += "loves #{ghost[:loves]} and "
+    ghost_info += "has #{ghost[:net_worth]} dollars in the bank."
+    puts ghost_info
+end
